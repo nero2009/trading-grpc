b677d976f1bec32b48fda5716101a1d97d48cedf
Adaware Nero <neroadaware@gmail.com>
2024-06-20 22:34:36 +0100

    implement client service to consume grpc api

--- client/client.go
+++ client/client.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"sort"
 
 	"github.com/nero2009/grpcTrader/trader"
 	"google.golang.org/grpc"
@@ -11,8 +14,34 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/toptraders", top10TradersHandler)
+	http.ListenAndServe(":8083", nil)
 
-	conn, err := grpc.Dial("localhost:9092", grpc.WithInsecure())
+	fmt.Print("Running leaderboard on port 9091")
+
+}
+
+func top10TradersHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := getTopTradersFromGRPC()
+	if err != nil {
+		http.Error(w, "Failed to get trading info", http.StatusInternalServerError)
+		return
+	}
+
+	sort.Slice(resp.Trades, func(i, j int) bool {
+		return resp.Trades[i].Price > resp.Trades[j].Price
+	})
+
+	top10Trades := resp.Trades[:10]
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(top10Trades)
+
+}
+
+func getTopTradersFromGRPC() (*trader.TradeResponse, error) {
+	conn, err := grpc.NewClient("localhost:9092", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to dial server: %v", err)
 	}
@@ -25,8 +54,5 @@ func main() {
 		log.Fatalf("Failed to get trading info: %v", err)
 	}
 
-	fmt.Println("Trading Info:")
-	for _, trade := range resp.Trades {
-		fmt.Printf("Stock Symbol: %s, Price: %f, Last Updated: %d, Trader Name: %s\n", trade.StockSymbol, trade.Price, trade.LastUpdated, trade.TraderName)
-	}
+	return resp, err
 }
